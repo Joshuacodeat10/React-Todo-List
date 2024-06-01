e0a3c142415884d3ff1ac71b6950227c475342fe
Joshua Agboola <joshuaagboola99@gmail.com>
2024-06-01 12:29:30 -0700

    Change onsubmit to use the addTodo function

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import TodoItem from './components/TodoItem'
-import TodoInput from './components/TodoInput'
+import TodoItem from './components/Todoitem/TodoItem'
+import TodoInput from './components/TodoInput/TodoInput'
 export default function App() {
-  const [todoList, setTodoList] = useState([{todoTitle:'',todoBody:'', todoStatus:''}])
+  
+  const [todoList, setTodoList] = useState([])
   function addTodo(event){
+    let TodoNumber = 1;
     event.preventDefault();
+    console.log()
     const newTodo = {
-      todoTitle:'nnn',
+      todoTitle: TodoNumber + 1,
       todoBody:'nnnn',
       todoStatus:'nnnn',
     }
+    console.log('boaou')
     setTodoList(c => [...c, newTodo])
   }
   return (
