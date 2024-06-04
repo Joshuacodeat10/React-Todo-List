7300e426ea774780ce61e4eb0b75d61142211cc7
Joshua Agboola <joshuaagboola99@gmail.com>
2024-06-04 09:02:06 -0700

    Added the onEdit function to fullFunc

--- src/components/Todoitem/TodoItem.jsx
+++ src/components/Todoitem/TodoItem.jsx
@@ -1,19 +1,23 @@
 import './todoitem.css'
 import { useState } from 'react'
 
-export default function TodoItem({itemNumber,itemTitle,  itemStatus, onDelete, editValue,submitEdit}){
+export default function TodoItem({itemNumber,itemTitle,  itemStatus, onDelete, editValue,submitEdit, changeEdit}){
     // const isEditing = useEditingState();
     // const onEdit = useEditingFunction();
-   
+    const [isEditing, setIsEditing]=  useState(false);
+    
     function Olpox(event){
         event.preventDefault();
+    }
+    function onEdit(){
+        setIsEditing(!isEditing)
     }
     function aLdenad(){
         alert('me')
     }
    
     function fullFunc(){
-        console.log('obobo')
+        onEdit();
         submitEdit()
     }
     return(
@@ -23,7 +27,7 @@ export default function TodoItem({itemNumber,itemTitle,  itemStatus, onDelete, e
             
             <p>{itemNumber}</p>
              <input type='checkbox'  defaultChecked={itemStatus}/>
-             {isEditing && <form onSubmit={fullFunc} ><input value={editValue} ></input><button type='submit' >submit</button></form>}
+             {isEditing && <form onSubmit={fullFunc} ><input value={editValue} onChange={changeEdit}></input><button type='submit' >submit</button></form>}
             {!isEditing && <h2>{itemTitle}</h2>}
             <button onClick={onEdit}>edit</button>
             <button onClick={onDelete}>delete</button>
